6b1451ccc1230e89cebac7952e00ce36a4eb5080
haesooKr <gotn676@gmail.com>
2020-06-12 05:15:30 -0400

    Code Refactoring for Buttons

--- haesooCalculator/ViewController.cs
+++ haesooCalculator/ViewController.cs
@@ -19,6 +19,7 @@ namespace haesooCalculator
             base.ViewDidLoad();
 
             // Do any additional setup after loading the view.
+            Console.WriteLine("Running");
         }
 
         public override NSObject RepresentedObject
@@ -34,54 +35,10 @@ namespace haesooCalculator
             }
         }
 
-        partial void Btn0(NSObject sender)
+        partial void BtnNum(NSObject sender)
         {
-            SetNum("0");
-        }
-
-        partial void Btn1(NSObject sender)
-        {
-            SetNum("1");
-        }
-
-        partial void Btn2(NSObject sender)
-        {
-            SetNum("2");
-        }
-
-        partial void Btn3(NSObject sender)
-        {
-            SetNum("3");
-        }
-
-        partial void Btn4(NSObject sender)
-        {
-            SetNum("4");
-        }
-
-        partial void Btn5(NSObject sender)
-        {
-            SetNum("5");
-        }
-
-        partial void Btn6(NSObject sender)
-        {
-            SetNum("6");
-        }
-
-        partial void Btn7(NSObject sender)
-        {
-            SetNum("7");
-        }
-
-        partial void Btn8(NSObject sender)
-        {
-            SetNum("8");
-        }
-
-        partial void Btn9(NSObject sender)
-        {
-            SetNum("9");
+            NSButton numButton = (NSButton)sender;
+            SetNum(numButton.Title);
         }
 
         partial void BtnPlus(NSObject sender)
--- haesooCalculator/ViewController.designer.cs
+++ haesooCalculator/ViewController.designer.cs
@@ -15,41 +15,14 @@ namespace haesooCalculator
 		[Outlet]
 		AppKit.NSTextField NumDisplay { get; set; }
 
-		[Action ("Btn0:")]
-		partial void Btn0 (Foundation.NSObject sender);
-
-		[Action ("Btn1:")]
-		partial void Btn1 (Foundation.NSObject sender);
-
-		[Action ("Btn2:")]
-		partial void Btn2 (Foundation.NSObject sender);
-
-		[Action ("Btn3:")]
-		partial void Btn3 (Foundation.NSObject sender);
-
-		[Action ("Btn4:")]
-		partial void Btn4 (Foundation.NSObject sender);
-
-		[Action ("Btn5:")]
-		partial void Btn5 (Foundation.NSObject sender);
-
-		[Action ("Btn6:")]
-		partial void Btn6 (Foundation.NSObject sender);
-
-		[Action ("Btn7:")]
-		partial void Btn7 (Foundation.NSObject sender);
-
-		[Action ("Btn8:")]
-		partial void Btn8 (Foundation.NSObject sender);
-
-		[Action ("Btn9:")]
-		partial void Btn9 (Foundation.NSObject sender);
-
 		[Action ("BtnEqual:")]
 		partial void BtnEqual (Foundation.NSObject sender);
 
 		[Action ("BtnMinus:")]
 		partial void BtnMinus (Foundation.NSObject sender);
+
+		[Action ("BtnNum:")]
+		partial void BtnNum (Foundation.NSObject sender);
 
 		[Action ("BtnPlus:")]
 		partial void BtnPlus (Foundation.NSObject sender);
